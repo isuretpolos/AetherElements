7deacb8f92494a90d52cb474a67a57628504406f
isuretpolos <isuret.polos@gmx.net>
2020-07-01 17:08:42 +0200

    Pom Modules and revision version tag

--- aether-shell/src/main/java/isuret/polos/aether/shell/AetherShell.java
+++ aether-shell/src/main/java/isuret/polos/aether/shell/AetherShell.java
@@ -3,21 +3,64 @@ package isuret.polos.aether.shell;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
 
+/**
+ * Based on text-io from Serban Iordache
+ * https://text-io.beryx.org/releases/latest/
+ */
 public class AetherShell {
 
-    public static void main(String [] args) {
-        TextIO textIO = TextIoFactory.getTextIO();
+    private TextIO textIO;
 
-        textIO.getTextTerminal().print("Aether Shell v1.0");
+    public static void main(String [] args) {
+        new AetherShell();
+    }
+
+    public AetherShell() {
+        textIO = TextIoFactory.getTextIO();
+        loop();
+    }
+
+    private void loop() {
+        textIO.getTextTerminal().setBookmark("title");
+        textIO.getTextTerminal().println("=== Aether Shell v1.0 ===");
+        textIO.getTextTerminal().println("-------------------------");
 
         while(true) {
-            AETHER_COMMANDS command = textIO.newEnumInputReader(AETHER_COMMANDS.class).read("What do you want to do?");
 
-            textIO.getTextTerminal().print("Your choice was " + command.name() + "\n");
+            AETHER_COMMANDS command = textIO.newEnumInputReader(AETHER_COMMANDS.class).read("");
+            textIO.getTextTerminal().println("Your choice was " + command.name());
 
             if (command.equals(AETHER_COMMANDS.EXIT)) break;
+            if (command.equals(AETHER_COMMANDS.GROUNDING)) {
+                grounding();
+            }
+
+            //textIO.getTextTerminal().resetToBookmark("title");
         }
 
         System.exit(0);
+    }
+
+
+    private void grounding() {
+        textIO.getTextTerminal().setBookmark("title");
+
+        for (int i=0; i< 40; i++) {
+            textIO.getTextTerminal().print("#");
+            delay();
+        }
+
+    }
+
+    private void delay() {
+        delay(200);
+    }
+
+    private void delay(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
